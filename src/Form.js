2bc8e61ce8e33a57bfb1055fb74f697c61c1b079
Orzu Tursunova <orzujke@gmail.com>
2022-11-05 15:17:56 -0500

    added submission alert and styling

--- src/Form.js
+++ src/Form.js
@@ -7,8 +7,15 @@ import {
   Select,
   MenuItem,
   Alert,
+  Dialog,
+  IconButton,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import axios from "axios";
+
+const inputStyle = {
+  WebkitBoxShadow: "0 0 0 1000px inherit inset",
+};
 
 const Form = () => {
   const [values, setValues] = useState({
@@ -19,6 +26,7 @@ const Form = () => {
     state: "",
   });
   const [data, setData] = useState({ occupations: [], states: [] });
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -37,6 +45,7 @@ const Form = () => {
         "https://frontend-take-home.fetchrewards.com/form",
         values
       );
+      setSuccess(true);
       setValues({
         name: "",
         email: "",
@@ -54,79 +63,125 @@ const Form = () => {
     setValues({ ...values, [ev.target.name]: ev.target.value });
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Form</h2>
-      <TextField
-        sx={{ width: 300 }}
-        required
-        type="text"
-        name="name"
-        label="Full Name"
-        onChange={handleChange}
-        value={values.name}
-        margin="dense"
-      />
-      <TextField
-        sx={{ width: 300 }}
-        required
-        type="email"
-        name="email"
-        label="E-mail"
-        onChange={handleChange}
-        value={values.email}
-        margin="dense"
-      />
-      <TextField
-        sx={{ width: 300 }}
-        required
-        type="password"
-        name="password"
-        label="Password"
-        onChange={handleChange}
-        value={values.password}
-        margin="dense"
-      />
-      <FormControl sx={{ mt: 1, width: 300 }}>
-        <InputLabel>Occupation</InputLabel>
-        <Select
+    <div>
+      <img src="../assets/logo.png" alt="Fetch Rewards logo"></img>
+      <form onSubmit={handleSubmit}>
+        <h2>Create an account</h2>
+        <TextField
+          sx={{ width: 300 }}
+          inputProps={{ style: inputStyle }}
           required
-          name="occupation"
-          value={values.occupation}
-          label="Occupation"
+          type="text"
+          name="name"
+          label="Full Name"
           onChange={handleChange}
+          value={values.name}
+          margin="dense"
+        />
+        <TextField
+          sx={{ width: 300 }}
+          inputProps={{ style: inputStyle }}
+          required
+          type="email"
+          name="email"
+          label="E-mail"
+          onChange={handleChange}
+          value={values.email}
+          margin="dense"
+        />
+        <TextField
+          sx={{ width: 300 }}
+          inputProps={{ style: inputStyle }}
+          required
+          type="password"
+          name="password"
+          label="Password"
+          onChange={handleChange}
+          value={values.password}
+          margin="dense"
+        />
+        <FormControl
+          sx={{ mt: 1, width: 300 }}
+          inputProps={{ style: inputStyle }}
         >
-          {data.occupations.map((occ) => {
-            return (
-              <MenuItem key={occ} value={occ}>
-                {occ}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
+          <InputLabel>Occupation</InputLabel>
+          <Select
+            required
+            name="occupation"
+            value={values.occupation}
+            label="Occupation"
+            onChange={handleChange}
+          >
+            {data.occupations.map((occ) => {
+              return (
+                <MenuItem key={occ} value={occ}>
+                  {occ}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
 
-      <FormControl sx={{ mt: 1, width: 300 }}>
-        <InputLabel>State</InputLabel>
-        <Select
-          required
-          name="state"
-          value={values.state}
-          label="State"
-          onChange={handleChange}
+        <FormControl
+          sx={{ mt: 1, width: 300 }}
+          inputProps={{ style: inputStyle }}
         >
-          {data.states.map((st) => {
-            return (
-              <MenuItem key={st.name} value={st.name}>
-                {st.name}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
-      <Button sx={{ mt: 1 }} variant="contained" type="submit">
-        Submit
-      </Button>
-    </form>
+          <InputLabel>State</InputLabel>
+          <Select
+            required
+            name="state"
+            value={values.state}
+            label="State"
+            onChange={handleChange}
+          >
+            {data.states.map((st) => {
+              return (
+                <MenuItem key={st.name} value={st.name}>
+                  {st.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <Button
+          sx={{
+            mt: 1,
+            backgroundColor: "rgb(255, 212, 82)",
+            color: "black",
+            "&:hover": { backgroundColor: "rgb(196, 99, 196)" },
+          }}
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+      {success ? (
+        <Dialog
+          open={success}
+          onClose={() => setSuccess(false)}
+          sx={{ height: 200 }}
+        >
+          <Alert
+            severity="success"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setSuccess(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+          >
+            Submission successful! Thank you!
+          </Alert>
+        </Dialog>
+      ) : null}
+    </div>
   );
 };
 
